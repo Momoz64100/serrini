c503d690e9311bb5a9f4eeb3717afd7f06999845
Momoz64100 <mickael.amorich@gmail.com>
2019-08-24 17:54:00 +0200

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAOwj46JPmBrRX2vO7DJTdcG9P7kxieTbU",
+    authDomain: "serrini-d1300.firebaseapp.com",
+    databaseURL: "https://serrini-d1300.firebaseio.com",
+    projectId: "serrini-d1300",
+    storageBucket: "serrini-d1300.appspot.com",
+    messagingSenderId: "82662109612",
+  }
 };
